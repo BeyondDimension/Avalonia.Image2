8917c8ee8233a7dc139a8bde7d2a8fc603c2a610
Aigio Liu <lysoft.pw@foxmail.com>
2023-02-16 10:39:36 +0800

    Misc

--- AvaloniaGif/ImageFormat.cs
+++ AvaloniaGif/ImageFormat.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-
-namespace AvaloniaGif;
+﻿namespace AvaloniaGif;
 
 internal enum ImageType
 {
@@ -14,36 +12,36 @@ internal enum ImageType
 
 internal static class ImageFormat
 {
-    public static ImageType GetImageType(this byte[] bytes)
+    public static ImageType GetImageType(this ReadOnlySpan<byte> bytes)
     {
         // see http://www.mikekunz.com/image_file_header.html  
-        var bmp = Encoding.ASCII.GetBytes("BM");     // BMP
-        var gif = Encoding.ASCII.GetBytes("GIF");    // GIF
-        var png = new byte[] { 137, 80, 78, 71 };    // PNG
-        var tiff = "II*"u8.ToArray();         // TIFF
-        var tiff2 = "MM*"u8.ToArray();         // TIFF
-        var jpeg = new byte[] { 255, 216, 255, 224 }; // jpeg
-        var jpeg2 = new byte[] { 255, 216, 255, 225 }; // jpeg canon
+        ReadOnlySpan<byte> bmp = "BM"u8;     // BMP
+        ReadOnlySpan<byte> gif = "GIF"u8;    // GIF
+        ReadOnlySpan<byte> png = new byte[] { 137, 80, 78, 71 };    // PNG
+        ReadOnlySpan<byte> tiff = "II*"u8;         // TIFF
+        ReadOnlySpan<byte> tiff2 = "MM*"u8;         // TIFF
+        ReadOnlySpan<byte> jpeg = new byte[] { 255, 216, 255, 224 }; // jpeg
+        ReadOnlySpan<byte> jpeg2 = new byte[] { 255, 216, 255, 225 }; // jpeg canon
 
-        if (bmp.SequenceEqual(bytes.Take(bmp.Length)))
+        if (bmp.SequenceEqual(bytes[..bmp.Length]))
             return ImageType.bmp;
 
-        if (gif.SequenceEqual(bytes.Take(gif.Length)))
+        if (gif.SequenceEqual(bytes[..gif.Length]))
             return ImageType.gif;
 
-        if (png.SequenceEqual(bytes.Take(png.Length)))
+        if (png.SequenceEqual(bytes[..png.Length]))
             return ImageType.png;
 
-        if (tiff.SequenceEqual(bytes.Take(tiff.Length)))
+        if (tiff.SequenceEqual(bytes[..tiff.Length]))
             return ImageType.tiff;
 
-        if (tiff2.SequenceEqual(bytes.Take(tiff2.Length)))
+        if (tiff2.SequenceEqual(bytes[..tiff2.Length]))
             return ImageType.tiff;
 
-        if (jpeg.SequenceEqual(bytes.Take(jpeg.Length)))
+        if (jpeg.SequenceEqual(bytes[..jpeg.Length]))
             return ImageType.jpeg;
 
-        if (jpeg2.SequenceEqual(bytes.Take(jpeg2.Length)))
+        if (jpeg2.SequenceEqual(bytes[..jpeg2.Length]))
             return ImageType.jpeg;
 
         return ImageType.unknown;
@@ -63,10 +61,12 @@ internal static class ImageFormat
         }
 
         TryReset(stream);
-        byte[] typedata = new byte[4];
-        stream.Read(typedata, 0, 4);
+        Span<byte> typedata = new byte[4];
+        stream.Read(typedata);
         TryReset(stream);
 
-        return typedata.GetImageType();
+        ReadOnlySpan<byte> typedata_ = typedata;
+
+        return typedata_.GetImageType();
     }
 }
